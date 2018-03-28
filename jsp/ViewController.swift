de3b64b482ae2ac260eae2c3c35a07e4be6ef157
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-03-28 16:34:06 +0900

    add 소수

--- jsp/ViewController.swift
+++ jsp/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var text: UITextField!
+    @IBOutlet var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        text.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func button(_ sender: Any) {
+        let number = Int(text.text!)
+        var a = true
+        if number! == 1{
+            a = false
+        }
+        if number != 1 && number != 2 {
+            for i  in 2 ..< number! {
+                if number! % i == 0 {
+                    a = false
+                }
+            }
+            }
+        if a == true{
+            label.text = "prine number"
+        }else{
+            label.text = "Not prine number"
+        }
+    }
+    
+    
+    @IBAction func button2(_ sender: Any) {
+        text.text = ""
+        label.text = ""
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        text.resignFirstResponder();
+        return true
+    }
 
 }
 
